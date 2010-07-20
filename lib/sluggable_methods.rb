5f8d2e36c10ad02fbf4380aa7e92f71c7c51284f
Brandon <brandon.leonardo@gmail.com>
2010-07-20 14:15:50 -0700

    formatting and add method to generate slug url

--- lib/sluggable_methods.rb
+++ lib/sluggable_methods.rb
@@ -36,20 +36,25 @@ module VentureHacks
 		# This module contains instance methods
 		module InstanceMethods
 			def create_slug_before_create
-					if slug_source && respond_to?(slug_source)
-						url_slug = send(slug_source).parameterize
+				if slug_source && respond_to?(slug_source)
+					url_slug = send(slug_source).parameterize
 
-						idx = 0
-						done_creating_slug = false
-						begin
-							slug_name = "#{url_slug}#{idx > 0 ? '-'+idx : ''}"
-							unless Slug.exists?(:name => slug_name)
-								build_slug :name => slug_name
-								done_creating_slug = true
-							end
-							idx = idx + 1
-						end while !done_creating_slug
-					end
+					idx = 0
+					done_creating_slug = false
+					begin
+						slug_name = "#{url_slug}#{idx > 0 ? '-'+idx.to_s : ''}"
+						unless Slug.exists?(:name => slug_name)
+							build_slug :name => slug_name
+							done_creating_slug = true
+						end
+						idx = idx + 1
+					end while !done_creating_slug
+				end
+			end
+
+			def slug_url(only_path=false)
+				domain = VentureHacks::SlugBugConfig.domain
+				url = "#{domain && !only_path ? domain : ''}/#{slug.name}"
 			end
 		end
 	end
